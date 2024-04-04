340cea9a6f0ad199350d58c3b7faa2a1709064dc
kangjy311 <kangjy311@naver.com>
2024-04-04 16:53:31 +0900

    feat: 게시판 이미지 firebase에 올리기

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import AuthRoute from './Routes/AuthRoute';
+import FeedPage from './pages/FeedPage/FeedPage';
 
 function App() {
   return (
     <>
-      // layout
+      {/* // layout
       // container
-      // sidebar
-      <AuthRoute/>
-
+      // sidebar */}
+      {/* <AuthRoute/> */}
+      <FeedPage />
     </>
   );
 }
