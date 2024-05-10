507704760ec669d99013eed8d69eb55a5519164e
elikese <elikese@gmail.com>
2024-05-10 20:27:47 +0900

    feat: response 받아와서 화면에 뿌림

--- src/pages/MypageFavoritePage/MypageFavoritePage.jsx
+++ src/pages/MypageFavoritePage/MypageFavoritePage.jsx
@@ -4,14 +4,16 @@ import FeedCard from "../../components/FeedCard/FeedCard";
 import * as s from "./style";
 
 import { useEffect, useState } from "react";
-import { getSavedBoard } from "../../apis/api/saveBoards";
+import { getSavedBoard, getSavedLunchBoard } from "../../apis/api/saveBoards";
+import LunchList from "../../components/LunchList/LunchList";
 
 export default function MypageFavoritePage() {
     const [viewState, setViewState] = useState(0); // 0 = feed, 1 = lunch
     const [feeds, setFeeds] = useState([]);
+    const [lunchs, setLunchs] = useState([]);
     const [isBookMarkClick, setIsBookMarkClick] = useState(true);
 
-    const getSavedBoardQuery = useQuery(["getSavedBoardQuery"], getSavedBoard, {
+    const getSavedBoardQuery = useQuery(["getSavedBoardQuery", isBookMarkClick], getSavedBoard, {
         retry: 0,
         refetchOnWindowFocus: false,
         onSuccess: (response) => {
@@ -22,9 +24,25 @@ export default function MypageFavoritePage() {
         },
     });
 
-    useEffect(() => {
-        getSavedBoardQuery.refetch();
-    }, [isBookMarkClick]);
+    const getSavedLunchBoardQuery = useQuery(["getSavedLunchBoardQuery"], getSavedLunchBoard, {
+        retry: 0,
+        refetchOnWindowFocus: false,
+        onSuccess: (response) => {
+            setLunchs(() =>
+                response.data.map((data) => {
+                    return {
+                        lunchId: data.lunchId,
+                        nickName: data.nickName,
+                        profileImgUrl: data.profileImgUrl,
+                        PlaceName: data.lunchPlaceName,
+                        categroies: data.lunchCategoryNames,
+                        title: data.lunchTitle,
+                        imgUrls: data.lunchImgUrls,
+                    };
+                })
+            );
+        },
+    });
 
     return (
         <>
@@ -43,7 +61,24 @@ export default function MypageFavoritePage() {
                             })}
                         </ul>
                     ) : (
-                        <div>lunch 콘텐츠 박스</div>
+                        <ul>
+                            {lunchs.map((lunch) => {
+                                return (
+                                    <div key={lunch.lunchId}>
+                                        <LunchList
+                                            lunchId={lunch.lunchId}
+                                            profileImgUrl={lunch.profileImgUrl}
+                                            nickName={lunch.nickName}
+                                            placeName={lunch.PlaceName}
+                                            categroies={lunch.categroies}
+                                            title={lunch.title}
+                                            imgUrls={lunch.imgUrls}
+                                            content={lunch.content}
+                                        />
+                                    </div>
+                                );
+                            })}
+                        </ul>
                     )}
                 </div>
             </div>
