40fbe63d4bc7ce008e1cd94abde84052938a6b74
dongyoon7212 <dongyoon7212@naver.com>
2024-05-10 16:03:59 +0900

    Feat: 중간 커밋

--- src/pages/EditAccountPage/EditAccountPage.jsx
+++ src/pages/EditAccountPage/EditAccountPage.jsx
@@ -1,140 +1,195 @@
 /** @jsxImportSource @emotion/react */
 import * as s from "./style";
-import React, { useEffect, useState } from 'react';
-import { useMutation, useQueryClient } from 'react-query';
-import { useNavigate } from 'react-router-dom';
-import instance from '../../apis/utils/instance';
-import { sendAuthMailRequest } from '../../apis/api/sendAuthMail';
+import React, { useEffect, useState } from "react";
+import { useMutation, useQueryClient } from "react-query";
+import { useNavigate } from "react-router-dom";
+import instance from "../../apis/utils/instance";
+import { sendAuthMailRequest } from "../../apis/api/sendAuthMail";
 import { useInput } from "../../hooks/useInput";
+import userImgNone from "../../assets/images/userProfileNone.png";
 import { duplicateUsername, editAccount } from "../../apis/api/account";
 
 export default function AccountEditPage() {
     const navigate = useNavigate();
     const queryClient = useQueryClient();
     const principalData = queryClient.getQueryData("principalQuery");
-    
-    const [username, userNameChange, usernameMessage, setUsernameValue, setUsernameMessage] = useInput("username");
-    const [ usernameCheck, setUsernameCheck ] = useState(false)
-    const [nickname, nicknameChange, nicknameMessage, setNicknameValue, setNicknameMessage] = useInput("nickname");
+
+    const [
+        username,
+        userNameChange,
+        usernameMessage,
+        setUsernameValue,
+        setUsernameMessage,
+    ] = useInput("username");
+    const [usernameCheck, setUsernameCheck] = useState(false);
+    const [
+        nickname,
+        nicknameChange,
+        nicknameMessage,
+        setNicknameValue,
+        setNicknameMessage,
+    ] = useInput("nickname");
     const [name, nameChange, nameMessage, setNameValue] = useInput("name");
     const [email, emailChange, emailMessage, setEmailValue] = useInput("email");
 
-    
-  
     // 이메일 인증하기
     const sendAuthMailMutation = useMutation({
-      mutationKey: "sendAuthMailMutation",
-      mutationFn: sendAuthMailRequest,
-      retry: 0,
-      onSuccess: (response) => {
-        if(response.data) {
-            alert("메일 전송을 완료하였습니다.");
-        }else {
-            alert("메일 전송에 실패하였습니다.");
-        }
-      }
+        mutationKey: "sendAuthMailMutation",
+        mutationFn: sendAuthMailRequest,
+        retry: 0,
+        onSuccess: (response) => {
+            if (response.data) {
+                alert("메일 전송을 완료하였습니다.");
+            } else {
+                alert("메일 전송에 실패하였습니다.");
+            }
+        },
     });
-  
+
     const handleSendAuthMailClick = () => {
-      sendAuthMailMutation.mutate();
-    }
-  
+        sendAuthMailMutation.mutate();
+    };
+
     // 로그아웃
     const handleLogoutClick = () => {
-  
-      localStorage.removeItem("AccessToken");
-      instance.interceptors.request.use((config) => {
-        config.headers.Authorization = null;
-        return config;
-      });
-      queryClient.refetchQueries("principalQuery");
-      window.location.replace("/auth/signin");
-    }
+        localStorage.removeItem("AccessToken");
+        instance.interceptors.request.use((config) => {
+            config.headers.Authorization = null;
+            return config;
+        });
+        queryClient.refetchQueries("principalQuery");
+        window.location.replace("/auth/signin");
+    };
 
-    const editAccountMutation = useMutation("editAccountMutation", editAccount, {
-      onSuccess: response => {
-        console.log(response)
-      },
-      onError: error => {
-        console.log(error)
-      }
-    })
+    const editAccountMutation = useMutation(
+        "editAccountMutation",
+        editAccount,
+        {
+            onSuccess: (response) => {
+                console.log(response);
+            },
+            onError: (error) => {
+                console.log(error);
+            },
+        }
+    );
 
-    const duplicateUsernameMutation = useMutation("duplicateUsernameMutation", duplicateUsername, {
-      onSuccess: response => {
-        setUsernameCheck(true)
-        alert("사용할 수 있는 아이디입니다.")
-      },
-      onError: error => {
-        alert(error.response.data.usernameCheck)
-      }
-    })
+    const duplicateUsernameMutation = useMutation(
+        "duplicateUsernameMutation",
+        duplicateUsername,
+        {
+            onSuccess: (response) => {
+                setUsernameCheck(true);
+                alert("사용할 수 있는 아이디입니다.");
+            },
+            onError: (error) => {
+                alert(error.response.data.usernameCheck);
+            },
+        }
+    );
 
     const editAccountButton = () => {
-      let changeUserInfo = {
-        userId: principalData.data.userId,
-        username: username,
-        nickname: nickname,
-        name: name,
-        email: email
-      }
-      if (usernameCheck === true) {
-        if(window.confirm("회원 정보를 변경하시겠습니까?") === true) {
-          editAccountMutation.mutate(changeUserInfo)
-          console.log(changeUserInfo)
+        let changeUserInfo = {
+            userId: principalData.data.userId,
+            username: username,
+            nickname: nickname,
+            name: name,
+            email: email,
+        };
+        if (usernameCheck === true) {
+            if (window.confirm("회원 정보를 변경하시겠습니까?") === true) {
+                editAccountMutation.mutate(changeUserInfo);
+                console.log(changeUserInfo);
+            }
+        } else {
+            alert("닉네임 중복 검사를 진행해주세요.");
         }
-      } else {
-        alert("닉네임 중복 검사를 진행해주세요.")
-      }
-    }
+    };
 
-    
     useEffect(() => {
-      setUsernameValue(principalData.data.username)
-      setNicknameValue(principalData.data.nickname)
-      setNameValue(principalData.data.name)
-      setEmailValue(principalData.data.email)
-    }, [principalData])
-    
+        setUsernameValue(principalData.data.username);
+        setNicknameValue(principalData.data.nickname);
+        setNameValue(principalData.data.name);
+        setEmailValue(principalData.data.email);
+    }, [principalData]);
+
     useEffect(() => {
-      setUsernameCheck(false)
-    }, [username])
+        setUsernameCheck(false);
+    }, [username]);
 
     const duplicateUsernameCheck = () => {
-      duplicateUsernameMutation.mutate(username)
-    }
-  
+        duplicateUsernameMutation.mutate(username);
+    };
+
     return (
-      <div css={s.accountEditPage}>
-        <div>
-            <label>사용자 이름: </label>
-            <input type="text" defaultValue={username} onChange={userNameChange}/>
-            <button onClick={duplicateUsernameCheck}>중복체크</button>
+        <div css={s.accountEditPage}>
+            <div css={s.title}>회원 정보</div>
+            <div css={s.main}>
+                <div css={s.profileImgBox}>
+                    <img src={userImgNone} alt="" css={s.profileImg} />
+                </div>
+                <div css={s.infoBox}>
+                    <div css={s.usernameBox}>
+                        <h1>{nickname}</h1>
+                        <h2>{email}</h2>
+                    </div>
+                </div>
+                <div css={s.editBox}>
+                    <div>
+                        <div css={s.labelBox}>
+                            <div css={s.label}>닉네임</div>
+                        </div>
+                        <div css={s.inputBox}>
+                            <input type="text" defaultValue={nickname} />
+                            <button css={s.checkButton}>중복확인</button>
+                        </div>
+                    </div>
+                    <div>
+                        <div css={s.labelBox}>
+                            <div css={s.label}>사용자 이름</div>
+                        </div>
+                        <div css={s.inputBox}>
+                            <input type="text" defaultValue={username} />
+                        </div>
+                    </div>
+                    <div>
+                        <div css={s.labelBox}>
+                            <div css={s.label}>이름</div>
+                        </div>
+                        <div css={s.inputBox}>
+                            <input type="text" defaultValue={name} />
+                        </div>
+                    </div>
+                    <div>
+                        <div css={s.labelBox}>
+                            <div css={s.label}>이메일</div>
+                        </div>
+                        <div css={s.inputBox}>
+                            <input type="text" defaultValue={email} />
+                            <button css={s.checkButton}>인증요청</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div css={s.buttonBox}>
+                <div onClick={() => navigate("/account/edit/password")}>
+                    비밀번호 변경
+                </div>
+                <div onClick={editAccountButton}>정보 수정</div>
+                <div onClick={handleLogoutClick}>로그아웃</div>
+            </div>
         </div>
-        <div>
-            <label>이름: </label>
-            <input type="text" defaultValue={name} onChange={nameChange}/>
-        </div>
-        <div>
-            <label>닉네임: </label>
-            <input type="text" defaultValue={nickname} onChange={nicknameChange}/>
-        </div>
-        <div>
-            <label>이메일: </label>
-            <input type="text" defaultValue={email} onChange={emailChange}/>
-          {
-            principalData?.data.authorities.filter(auth => auth.authority === "ROLE_USER").length === 0
-              ?
-              <button onClick={handleSendAuthMailClick}>인증하기</button>
-              :
-              <div></div>
-          }
-        </div>
-        <div css={s.accountEditButtons}>
-          <button onClick={editAccountButton}>정보 수정</button>
-          <button onClick={() => navigate("/account/edit/password")}>비밀번호 수정</button>
-          <button onClick={handleLogoutClick}>로그아웃</button>
-        </div>
-      </div>
     );
-  }
+}
+
+// <div>
+//     <label>이메일: </label>
+//     <input type="text" defaultValue={email} onChange={emailChange} />
+//     {principalData?.data.authorities.filter(
+//         (auth) => auth.authority === "ROLE_USER"
+//     ).length === 0 ? (
+//         <button onClick={handleSendAuthMailClick}>인증하기</button>
+//     ) : (
+//         <div></div>
+//     )}
+// </div>;
--- src/pages/EditAccountPage/style.js
+++ src/pages/EditAccountPage/style.js
@@ -1,15 +1,151 @@
 import { css } from "@emotion/react";
 
 export const accountEditPage = css`
-  box-sizing: border-box;
-  padding: 30px 0;
-  width: 700px;
+    box-sizing: border-box;
+    width: 700px;
+    border-radius: 5px;
+    border: 2px solid #8a9c99;
+    background-color: #f6f5ef;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`;
 
-`
+export const title = css`
+    width: 100%;
+    height: 70px;
+    border-bottom: 2px solid #8a9c99;
+    font-size: 40px;
+    font-weight: 400;
+    text-align: center;
+    line-height: 70px;
+`;
 
-export const accountEditButtons = css`
-  width: 100%;
-  button {
-    
-  }
-`
+export const main = css`
+    width: 100%;
+    height: 70%;
+`;
+
+export const profileImgBox = css`
+    width: 100%;
+    height: 200px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const profileImg = css`
+    width: 35%;
+    border-radius: 50%;
+    border: 2px solid #8a9c99;
+`;
+
+export const infoBox = css`
+    width: 100%;
+    height: 100px;
+    display: flex;
+    flex-direction: column;
+`;
+
+export const usernameBox = css`
+    padding-top: 12px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    & > h1 {
+        font-size: 40px;
+        font-weight: 600;
+    }
+    & > h2 {
+        margin-top: 10px;
+        font-size: 15px;
+        color: #22222266;
+    }
+`;
+
+export const editBox = css`
+    width: 100%;
+    height: 300px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    & > div {
+        display: flex;
+    }
+`;
+
+export const labelBox = css`
+    width: 40%;
+    height: 50px;
+    display: flex;
+    justify-content: right;
+    align-items: center;
+`;
+
+export const label = css`
+    font-size: 20px;
+`;
+
+export const checkButton = css`
+    position: absolute;
+    right: 15px;
+    background-color: transparent;
+    border: none;
+    font-size: 14px;
+    font-weight: 500;
+`;
+
+export const inputBox = css`
+    position: relative;
+    width: 60%;
+    height: 50px;
+    display: flex;
+    align-items: center;
+
+    & > input {
+        margin-left: 5px;
+        width: 300px;
+        height: 20px;
+        font-size: 20px;
+        padding: 5px;
+        background-color: transparent;
+        outline: none;
+        border: none;
+        border-bottom: 1px solid #dbdbdb;
+    }
+`;
+
+export const buttonBox = css`
+    box-sizing: border-box;
+    border-top: 2px solid #cad8d8;
+    width: 100%;
+    height: 50px;
+    text-align: center;
+    line-height: 50px;
+    font-size: 18px;
+    font-weight: 500;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    & > div:nth-of-type(1) {
+        box-sizing: border-box;
+        width: 30%;
+        height: 100%;
+        border-right: 2px solid #cad8d8;
+    }
+    & > div:nth-of-type(2) {
+        box-sizing: border-box;
+        width: 40%;
+        height: 100%;
+        border-right: 2px solid #cad8d8;
+    }
+    & > div:nth-of-type(3) {
+        width: 30%;
+        height: 100%;
+    }
+`;
