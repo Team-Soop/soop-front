1c7a9622cfdab379e82bd05b00a9fa71e259cb2e
dongyoon7212 <dongyoon7212@naver.com>
2024-05-10 17:12:41 +0900

    Feat: 마이페이지 피드 수정

--- src/pages/MypageFeed/MypageFeed.jsx
+++ src/pages/MypageFeed/MypageFeed.jsx
@@ -1,47 +1,55 @@
 /** @jsxImportSource @emotion/react */
 import * as s from "./style";
-import { useMutation, useQuery, useQueryClient } from 'react-query';
-import FeedCard from '../../components/FeedCard/FeedCard';
+import { useMutation, useQuery, useQueryClient } from "react-query";
+import FeedCard from "../../components/FeedCard/FeedCard";
 import { useState } from "react";
 import { getMypageFeedList } from "../../apis/api/feed";
+import { IoAlert } from "react-icons/io5";
 
 export default function MypageFeed() {
-  const queryClient = useQueryClient();
-  const principalData = queryClient.getQueryData("principalQuery");
-  const [ mypageFeedList, setMypageFeedList ] = useState([]);
+    const queryClient = useQueryClient();
+    const principalData = queryClient.getQueryData("principalQuery");
+    const [mypageFeedList, setMypageFeedList] = useState([]);
 
-  // 마이 페이지 피드 리스트 get
-  const getmypageFeedListQuery = useQuery(
-    "getmypageFeedListQuery", 
-    () => getMypageFeedList(principalData.data.userId),
-    {
-      refetchOnWindowFocus: false,
-      retry: 0,
-      onSuccess: response => {
-        console.log("피드 리스트 성공");
-        setMypageFeedList(response.data);
-        console.log(response);
-      },
-      onError: error => {
-        console.log("피드 리스트 오류");
-        console.log(error);
-      }
-    }
-  );
+    // 마이 페이지 피드 리스트 get
+    const getmypageFeedListQuery = useQuery(
+        "getmypageFeedListQuery",
+        () => getMypageFeedList(principalData.data.userId),
+        {
+            refetchOnWindowFocus: false,
+            retry: 0,
+            onSuccess: (response) => {
+                console.log("피드 리스트 성공");
+                setMypageFeedList(response.data);
+                console.log(response);
+            },
+            onError: (error) => {
+                console.log("피드 리스트 오류");
+                console.log(error);
+            },
+        }
+    );
 
-  return (
-      <div css={s.mypageFeedRootLayout}>
-        {mypageFeedList.length > 0 
-        ? 
-          (
-            <ul>
-              {
-                mypageFeedList.map(feed => <FeedCard feed={feed} key={feed.feedId}/>
-              )}
-            </ul>
-          )
-        : <div>작성한 게시글이 없습니다.</div>
-      }
-      </div>
-  )
+    return (
+        <>
+            {mypageFeedList.length > 0 ? (
+                <div css={s.mypageFeedRootLayout}>
+                    <ul>
+                        {mypageFeedList.map((feed) => (
+                            <FeedCard feed={feed} key={feed.feedId} />
+                        ))}
+                    </ul>
+                </div>
+            ) : (
+                <div css={s.root}>
+                    <div>
+                        <div>
+                            <IoAlert />
+                        </div>
+                        <h1>작성한 게시글이 없습니다.</h1>
+                    </div>
+                </div>
+            )}
+        </>
+    );
 }
--- src/pages/MypageFeed/style.js
+++ src/pages/MypageFeed/style.js
@@ -1,17 +1,46 @@
 import { css } from "@emotion/react";
 
 export const mypageFeedRootLayout = css`
-  box-sizing: border-box;
-  position: relative;
-  width: 100%;
-  height: 100%;
+    box-sizing: border-box;
+    position: relative;
+    width: 100%;
+    height: 100%;
 `;
 
 export const mypageFeedLayout = css`
-  width: 100%;
-  height: 100%;
-  overflow-y: auto;
-  &::-webkit-scrollbar {
-    display: none;
-  }
+    width: 100%;
+    height: 100%;
+    overflow-y: auto;
+    &::-webkit-scrollbar {
+        display: none;
+    }
+`;
+
+export const root = css`
+    width: 700px;
+    height: 100%;
+    border-radius: 5px;
+    box-sizing: border-box;
+    border: 2px solid #8a9c99;
+    background-color: #f6f5ef;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    & > div {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    & > div > div {
+        margin-top: 6px;
+        color: red;
+        font-size: 33px;
+    }
+
+    & > div > h1 {
+        font-size: 30px;
+        font-weight: 400;
+    }
 `;
